a273836f88810b2ef956006d46d8bc2a7454f162
mtlan <maitruclan99@gmail.com>
2025-07-13 08:18:21 +0700

    fix

--- config/livewire.php
+++ config/livewire.php
@@ -79,7 +79,7 @@ return [
 
     'temporary_file_upload' => [
         'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => ['file', 'mimes:jpg,jpeg,png,gif,pdf,doc,docx,xls,xlsx,mp4,mov,avi,wmv,txt,zip', 'max:102400'],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'rules' => ['file', 'mimes:jpg,jpeg,png,gif,pdf,doc,docx,xls,xlsx,mp4,mov,avi,wmv,txt,zip', 'max:10240'],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
